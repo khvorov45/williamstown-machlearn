9250430fc4474ee99b5e838b80e23f90b287a398
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-06 09:59:03 +1100

    added tree predictions

--- tree/tree.R
+++ tree/tree.R
@@ -22,10 +22,37 @@ read_data <- function(name) {
   )
 }
 
+read_data_test <- function(name) {
+  read_data(name) %>%
+    select(age, sex, los_real = los, -starts_with("log_los"))
+}
+
 save_tree <- function(tree, name) {
   pdf(file.path(tree_dir, glue::glue("{name}.pdf")))
   prp(tree)
   dev.off()
+}
+
+add_predicted <- function(tree, newdata) {
+  newdata %>%
+    mutate(
+      los_predicted = predict(tree, newdata)
+    )
+}
+
+plot_scatter_acc <- function(preds) {
+  preds %>%
+    ggplot(aes(los_predicted, los_real)) +
+    dark_theme_bw(verbose = FALSE) +
+    geom_jitter()
+}
+
+save_plot <- function(plot, name) {
+  ggsave_dark(
+    file.path(tree_dir, glue::glue("{name}.pdf")),
+    plot, dark = FALSE,
+    width = 7.5, height = 7.5, units = "cm"
+  )
 }
 
 # Script ======================================================================
@@ -45,3 +72,14 @@ tree_miss <- rpart(los ~ age + sex, sim_miss)
 save_tree(tree_nomiss, "miss")
 
 # Predictions
+sim_nomiss_test <- read_data_test("sim-nomiss-test")
+sim_miss_test <- read_data_test("sim-miss-test")
+
+sim_nomiss_pred <- add_predicted(tree_nomiss, sim_nomiss_test)
+sim_miss_pred <- add_predicted(tree_miss, sim_miss_test)
+
+sc_nomiss <- plot_scatter_acc(sim_nomiss_pred)
+save_plot(sc_nomiss, "sc-nomiss")
+
+sc_miss <- plot_scatter_acc(sim_miss_pred)
+save_plot(sc_miss, "sc-miss")
