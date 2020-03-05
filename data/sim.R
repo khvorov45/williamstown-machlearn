ca697ceb2dcfd55091cb793420b435fabcc14fc5
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-05 14:46:35 +1100

    added simulation

---
+++ data/sim.R
@@ -0,0 +1,59 @@
+# Simulated data
+# Arseniy Khvorov
+
+library(tidyverse)
+library(extraDistr)
+
+# Directories used
+data_dir <- "data"
+
+# Functions ===================================================================
+
+sim_dat <- function(n = 1000) {
+  tibble(
+    age = runif(n, 20, 80),
+    male = as.integer(rbern(n, 0.5)),
+    log_los = 1 + 0.03 * age + 0.02 * male - 0.01 * age * male
+  )
+}
+
+cond_missing <- function(vec, pna, naval) {
+  if_else(as.logical(rbern(length(vec), pna)), naval, vec)
+}
+
+add_miss <- function(dat, pna = 0.2) {
+  dat %>%
+    mutate(
+      age = cond_missing(age, pna, NA_real_),
+      male = cond_missing(male, pna, NA_integer_)
+    )
+}
+
+reformat_vars <- function(dat) {
+  dat %>%
+    mutate(
+      sex = if_else(male == 1L, "male", "female"),
+      los = as.integer(exp(log_los))
+    )
+}
+
+save_data <- function(dat, name) {
+  write_csv(dat, file.path(data_dir, glue::glue("{name}.csv")))
+}
+
+gen_dat <- function(n, pna, name) {
+  sim_dat(n) %>%
+    add_miss(pna) %>%
+    reformat_vars() %>%
+    save_data(name)
+}
+
+# Script ======================================================================
+
+example <- sim_dat(15) %>% reformat_vars()
+
+gen_dat(1e3, 0, "sim-nomiss")
+gen_dat(1e3, 0.5, "sim-miss")
+
+gen_dat(1e3, 0, "sim-nomiss-test")
+gen_dat(1e3, 0.5, "sim-miss-test")
